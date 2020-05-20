088abbb9d0913c49064f9117700ff70e0bb0eac3
김선영 <tjsgur35@betree.co.kr>
2020-05-20 13:44:53 +0900

    server setting

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI : 'mongodb+srv://seonn:tkanfkdl!@34@cluster0-t9yuh.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+}).then(() => console.log('MongoDB conneted...')).catch(err => console.log(err));
+
+
+
+app.get('/', (req, res) => res.send('Hello World! 안녕'));
+
+// 회원가입
+app.post('/register', (req, res) => {
+	const user = new User(req.body);
+	user.save((err, userInfo) => {
+		if(err) return res.json({success: false, err});
+		return res.status(200).json({
+			success: true,
+		})
+	})
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
